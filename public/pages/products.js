49653916bdce3e5b945cff74926f855d051256f4
Perfect Aimers Enterprise <perfectaimersenterprise@gmail.com>
2023-12-21 05:12:39 +0100

    deploying by 5:13

--- public/pages/products.js
+++ public/pages/products.js
@@ -1,3 +1,9 @@
 const emoji = document.querySelector('.emoji');
 const emo = '💸';
 emoji.innerText = emo;
+
+const uglybtn = document.querySelector('.uglybtn')
+
+uglybtn.onclick = () => {
+    window.location.href = 'products.html'
+}
